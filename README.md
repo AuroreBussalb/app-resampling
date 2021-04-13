bd8ca73c8aa094a08745cdf9d25bee1ca785f02f
aurore.bussalb <aurore.bussalb@mensiatech.com>
2021-04-13 15:14:26 +0200

    app resampling

--- README.md
+++ README.md
@@ -1,64 +1,29 @@
 # app-resampling
 
-This is a draft of a future Brainlife App that filters MEG signals using the MNE functions: 
-[`mne.io.Raw.filter`](https://mne.tools/stable/generated/mne.io.Raw.html#mne.io.Raw.filter), 
-[`mne.io.Raw.notch_filter`](https://mne.tools/stable/generated/mne.io.Raw.html#mne.io.Raw.notch_filter), 
-[`mne.io.Raw.resample`](https://mne.tools/stable/generated/mne.io.Raw.html#mne.io.Raw.resample).
+This is a draft of a future Brainlife App that resamples MEG signals using the MNE functions:  
+[`mne.io.Raw.resample`](https://mne.tools/stable/generated/mne.io.Raw.html#mne.io.Raw.resample) or 
+[`mne.Epochs.resample`](https://mne.tools/stable/generated/mne.Epochs.html?highlight=mne%20epochs#mne.Epochs.resample).
 
-# app-temporal-filtering documentation
+# app-resampling documentation
 
-1) Filter MEG signals
-2) First, apply a bandpass, highpass, or lowpass filter, then optionally apply a notch filter and resample the data  
-3) Input file is:
+1) Resample MEG signals 
+2) Input file is:
     * a MEG file in `.fif` format,
-4) Input parameters are:
-    * filter_l_freq: `float`, optional, for FIR filters, the lower pass-band edge; for IIR filters, the lower cutoff frequency. If `None` the data are only low-passed.  
-    * filter_h_freq: `float`, optional, for FIR filters, the upper pass-band edge; for IIR filters, the upper cutoff frequency. If `None` the data are only high-passed.
-    * filter_picks: `str`or `list`, optional, channels to include. Default is `None`.
-    * filter_length: `str`, length of the FIR filter to use in human-readable time units. Default is `auto`. 
-    * filter_l_trans_bandwidth: `float` or `str`, width of the transition band at the low cut-off frequency in Hz. Default is `auto`.
-    * filter_h_trans_bandwidth: `float` or `str`, width of the transition band at the high cut-off frequency in Hz. Default is `auto`.
-    * filter_n_jobs: `int`, number of jobs to run in parallel. Default is 1. 
-    * filter_method: `str`, 'fir' will use overlap-add FIR filtering, 'iir' will use IIR forward-backward filtering. Default is 'fir'.
-    * filter_iir_params: `dict`, optional, dictionary of parameters to use for IIR filtering. To know how to define the dictionary go 
-        [there](https://mne.tools/stable/generated/mne.filter.construct_iir_filter.html#mne.filter.construct_iir_filter). Default is `None`.
-    * filter_phase: `str`, phase of the filter. Default is 'zero'.
-    * filter_fir_window: `str`, the window to use in FIR design. Default is 'hamming'.
-    * filter_fir_design: `str`. Default is `firwin`.
-    * filter_skip_by_annotation: `str` or `list of str`, if a string (or list of str), any annotation segment that begins with the given string will not be included in
-        filtering, and segments on either side of the given excluded annotated segment will be filtered separately. Default is `["edge", bad_acq_skip"]`.
-    * filter_pad: `str`, the type of padding to use. Default is 'reflect_limited'.
-    * apply_notch: `bool`, if `True`, apply a notch filter. Default is `True`.
-    * notch_freqs_start: `int`, frequency to notch filter in Hz. Default is 50.
-    * notch_freqs_end: `int`, end of the interval (in Hz) of the power lines harmonics to notch filter (this value is excluded). Default is 251.  
-    * notch_freqs_step: `int`, The step in Hz to filter power lines harmonics between param_notch_freqs_start and param_notch_freqs_end. Default is 50.
-    * notch_picks: `str`or `list`, optional, channels to include. Default is `None`. 
-    * notch_filter_length: `str`, length of the FIR filter to use in human-readable time units. Default is `auto`. 
-    * notch_widths: `float`, optional, width of the stop band in Hz. Default is `None`.
-    * notch_trans_bandwidth: `float`, width of the transition band in Hz. Default is 1.0.
-    * notch_n_jobs: `int`, number of jobs to run in parallel. Default is 1.
-    * notch_method: `str`, 'fir' will use overlap-add FIR filtering, 'iir' will use IIR forward-backward filtering. Default is 'fir'.
-    * notch_iir_params: `dict`, optional, dictionary of parameters to use for IIR filtering. To know how to define the dictionary go 
-        [there](https://mne.tools/stable/generated/mne.filter.construct_iir_filter.html#mne.filter.construct_iir_filter). Default is `None`. 
-    * notch_mt_bandwidth: `float`, optional, the bandwidth of the multitaper windowing function in Hz. Default is `None`.
-    * notch_p_value: `float`, p-value to use in F-test thresholding to determine significant sinusoidal components. Default is 0.05.
-    * notch_phase: `str`, phase of the filter, only used if method='fir'. Default is 'zero'.
-    * notch_fir_window: `str`, the window to use in FIR design. Default is 'hamming'.
-    * notch_fir_design: `str`. Default is 'firwin'.
-    * notch_pad: `str`, the type of padding to use. Default is 'reflect_limited'.
-    * apply_resample: `bool`, if True resample the data. Default is `True`.
+3) Input parameters are:
+    * param_epoched_data: `bool`, if True, the data to be resampled is epoched, else it is continuous.
     * param_resample_sfreq: `float`, new sample rate to use.
     * param_resample_npad: `int` or `str`, amount to pad the start and end of the data. Default is 'auto'.
     * param_resample_window: `str`, frequency-domain window to use in resampling. Default is `boxcar`. 
     * param_resample_stim_picks: `list of /*int` or `None`, stim channels. Default is `None`.
     * param_resample_n_jobs: `int`, number of jobs to run in parallel. Default is 1. 
     * param_resample_events: `2D array, shape (n_events, 3)`, optional, an optional event matrix. Default is `None`.
-    * param_resample_pad: `str`, the type of padding to use. Default is 'reflect_limited'. 
+    * param_raw_pad: `str`, the type of padding to use for raw data. Default is 'reflect_limited'. 
+    * param_epoch_pad: `str`, the type of padding to use for epoched data. Default is 'edge'. 
 
 This list along with the parameters' default values correspond to the 0.22.0 version of MNE Python.  
 
 5) Ouput files are:
-    * a `.fif` MEG file after filtering,
+    * a `.fif` MEG file after resampling,
     * an `.html` report containing figures.
 
 ### Authors
--- resampling.py
+++ resampling.py
@@ -5,151 +5,64 @@ import mne
 import numpy as np
 
 
-def temporal_filtering(raw, param_filter_l_freq, param_filter_h_freq, param_filter_picks, param_filter_length,
-                       param_filter_l_trans_bandwidth, param_filter_h_trans_bandwidth, param_filter_n_jobs,
-                       param_filter_method, param_filter_iir_params, param_filter_phase, param_filter_fir_window,
-                       param_filter_fir_design, param_filter_skip_by_annotation, param_filter_pad, param_apply_notch,
-                       param_notch_freqs_start, param_notch_freqs_end, param_notch_freqs_step, param_notch_picks,
-                       param_notch_filter_length, param_notch_widths, param_notch_trans_bandwidth, param_notch_n_jobs,
-                       param_notch_method, param_notch_iir_parameters, param_notch_mt_bandwidth, param_notch_p_value,
-                       param_notch_phase, param_notch_fir_window, param_notch_fir_design, param_notch_pad,
-                       param_apply_resample, param_resample_sfreq, param_resample_npad, param_resample_window,
-                       param_resample_stim_picks, param_resample_n_jobs, param_resample_events, param_resample_pad):
-    """Perform filtering using MNE Python and save the file once filtered.
+def resampling(data, param_sfreq, param_npad, param_window,
+               param_stim_picks, param_n_jobs, param_events, param_pad):
+    """Resample the signals using MNE Python and save the file once resampled.
 
     Parameters
     ----------
-    raw: instance of mne.io.Raw
-        Data to be filtered.
-    param_filter_l_freq: float or None
-        For FIR filters, the lower pass-band edge; for IIR filters, the lower cutoff frequency. If None the 
-        data are only low-passed.
-    param_filter_h_freq: float or None
-        For FIR filters, the upper pass-band edge; for IIR filters, the upper cutoff frequency. If None the 
-        data are only high-passed.
-    param_filter_picks: str, list, or None
-        Channels to include.
-    param_filter_length: str
-        Length of the FIR filter to use (if applicable). Can be ‘auto’ (default) : the filter length is chosen based 
-        on the size of the transition regions, or an other str (human-readable time in units of “s” or “ms”: 
-        e.g., “10s” or “5500ms”). 
-    param_filter_l_trans_bandwidth: float or str
-        Width of the transition band at the low cut-off frequency in Hz (high pass or cutoff 1 in bandpass). 
-        Can be “auto” (default) to use a multiple of l_freq.     
-    param_filter_h_trans_bandwidth: float or str   
-        Width of the transition band at the high cut-off frequency in Hz (low pass or cutoff 2 in bandpass). 
-        Can be “auto” (default) to use a multiple of h_freq.
-    param_filter_n_jobs: int
-        Number of jobs to run in parallel.
-    param_filter_method: str
-        ‘fir’ will use overlap-add FIR filtering, ‘iir’ will use IIR forward-backward filtering (via filtfilt).
-    param_filter_iir_params: dict or None
-        Dictionary of parameters to use for IIR filtering. If iir_params is None and method=”iir”, 
-        4th order Butterworth will be used. 
-    param_filter_phase: str
-        Phase of the filter, only used if method='fir'. Either 'zero' or 'zero-double'.
-    param_filter_fir_window: str
-        The window to use in FIR design, can be “hamming” (default), “hann” (default in 0.13), or “blackman”.
-    param_filter_fir_design: str
-        Can be “firwin” (default) or “firwin2”.
-    param_filter_skip_by_annotation: str or list of str
-        If a string (or list of str), any annotation segment that begins with the given string will not be included in
-        filtering, and segments on either side of the given excluded annotated segment will be filtered separately.
-    param_filter_pad: str
-        The type of padding to use. Supports all numpy.pad() mode options. Can also be “reflect_limited” (default).
-    param_apply_notch: bool
-        If True apply a notch filter.
-    param_notch_freqs_start: int
-        Frequency to notch filter in Hz.
-    param_notch_freqs_end: int
-        End of the interval (in Hz) of the power lines harmonics to notch filter. This value is excluded.
-    param_notch_freqs_step: int
-        The step in Hz to filter power lines harmonics between param_notch_freqs_start and param_notch_freqs_end.
-    param_notch_picks: list, or None
-        Channels to include.
-    param_notch_filter_length: str
-        Length of the FIR filter to use (if applicable). Can be ‘auto’ (default) : the filter length is chosen based 
-        on the size of the transition regions, or an other str (human-readable time in units of “s” or “ms”: 
-        e.g., “10s” or “5500ms”. 
-    param_notch_widths: float or None
-        Width of the stop band in Hz. If None, freqs / 200 is used.
-    param_notch_trans_bandwidth: float
-        Width of the transition band in Hz. 
-    param_notch_n_jobs: int
-        Number of jobs to run in parallel.
-    param_notch_method: str
-        ‘fir’ will use overlap-add FIR filtering, ‘iir’ will use IIR forward-backward filtering (via filtfilt). 
-    param_notch_iir_parameters: dict or None
-        Dictionary of parameters to use for IIR filtering. If iir_params is None and method=”iir”, 
-        4th order Butterworth will be used.
-    param_notch_mt_bandwidth: float or None
-        The bandwidth of the multitaper windowing function in Hz.
-    param_notch_p_value: float
-        P-value to use in F-test thresholding to determine significant sinusoidal components 
-        to remove when method=’spectrum_fit’ and freqs=None.
-    param_notch_phase: str
-        Phase of the filter, only used if method='fir'. Either 'zero' or 'zero-double'.
-    param_notch_fir_window: str
-        The window to use in FIR design, can be “hamming” (default), “hann”, or “blackman”.
-    param_notch_fir_design: str
-        Can be “firwin” (default) or “firwin2”.
-    param_notch_pad: str
-        The type of padding to use. Supports all numpy.pad() mode options. Can also be “reflect_limited” (default).
-    param_apply_resample: bool
-        If True resample the data.
-    param_resample_sfreq: float
+    data: instance of mne.io.Raw or instance of mne.Epochs
+        Data to be resampled.
+    param_epoched_data: bool
+        If True, the data to be resampled is epoched, else it is continuous.
+    param_sfreq: float
         New sample rate to use.
-    param_resample_npad: int or str
+    param_npad: int or str
         Amount to pad the start and end of the data. Can be “auto” (default).
-    param_resample_window: str
+    param_window: str
         Frequency-domain window to use in resampling. 
-    param_resample_stim_picks: list of int or None
+    param_stim_picks: list of int or None
         Stim channels.
-    param_resample_n_jobs: int
+    param_n_jobs: int
         Number of jobs to run in parallel.
-    param_resample_events: 2D array, shape (n_events, 3) or None
+    param_events: 2D array, shape (n_events, 3) or None
         An optional event matrix. 
-    param_resample_pad: str
-        The type of padding to use. Supports all numpy.pad() mode options. Can also be “reflect_limited” (default).
+    param_raw_pad: str
+        The type of padding to use for raw data. Supports all numpy.pad() mode options. Can also be 
+        “reflect_limited” (default).
+    param_epoch_pad: str
+        The type of padding to use for epoched data. Supports all numpy.pad() mode options. Can also be 
+        “reflect_limited” or "edge" (default).
 
     Returns
     -------
-    raw_filtered: instance of mne.io.Raw
-        The raw data after filtering.
+    raw_filtered: instance of mne.io.Raw or instance of mne.Epochs
+        The raw data after resampling.
     """
 
-    raw.load_data()
+    # For continuous data 
+    if param_epoched_data is False:
 
-    # Bandpass, lowpass, or highpass filter
-    raw_filtered = raw.filter(l_freq=param_filter_l_freq, h_freq=param_filter_h_freq, 
-                              picks=param_filter_picks, filter_length=param_filter_length,
-                              l_trans_bandwidth=param_filter_l_trans_bandwidth,
-                              h_trans_bandwidth=param_filter_h_trans_bandwidth, n_jobs=param_filter_n_jobs,
-                              method=param_filter_method, iir_params=param_filter_iir_params, phase=param_filter_phase,
-                              fir_window=param_filter_fir_window, fir_design=param_filter_fir_design,
-                              skip_by_annotation=param_filter_skip_by_annotation, pad=param_filter_pad)
+        # Load data
+        data.load_data()
 
-    # Notch
-    if param_apply_notch is True:
-        freqs = np.arange(param_notch_freqs_start, param_notch_freqs_end, param_notch_freqs_step)
-        raw_filtered.notch_filter(freqs=freqs, picks=param_notch_picks,
-                                  filter_length=param_notch_filter_length, notch_widths=param_notch_widths,
-                                  trans_bandwidth=param_notch_trans_bandwidth, n_jobs=param_notch_n_jobs,
-                                  method=param_notch_method, iir_params=param_notch_iir_parameters,
-                                  mt_bandwidth=param_notch_mt_bandwidth, p_value=param_notch_p_value,
-                                  phase=param_notch_phase, fir_window=param_notch_fir_window,
-                                  fir_design=param_notch_fir_design, pad=param_notch_pad)
+        # Resample data
+        data_resampled = data.resample(sfreq=param_sfreq, npad=param_npad, window=param_window,
+                                       stim_picks=param_stim_picks, n_jobs=param_n_jobs,
+                                       events=param_events, pad=param_raw_pad)
 
-    # Resample
-    if param_apply_resample is True:
-        raw_filtered.resample(sfreq=param_resample_sfreq, npad=param_resample_npad, window=param_resample_window,
-                              stim_picks=param_resample_stim_picks, n_jobs=param_resample_n_jobs,
-                              events=param_resample_events, pad=param_resample_pad)
+    # For epoched data 
+    else:
+
+        # Resample data
+        data_resampled = data.resample(sfreq=param_resample_sfreq, npad=param_resample_npad, 
+                                       window=param_resample_window, n_jobs=param_resample_n_jobs, 
+                                       pad=param_epoch_pad)
 
     # Save file
-    raw.save("out_dir_temporal_filtering/meg.fif", overwrite=True)
+    data_resampled .save("out_dir_resampling/meg.fif", overwrite=True)
 
-    return raw_filtered
+    return data_resampled 
 
 
 def _compute_snr(meg_file):
@@ -329,106 +242,37 @@ def main():
 
     # Read the files
     data_file = config.pop('fif')
-    raw = mne.io.read_raw_fif(data_file, allow_maxshield=True)
-
-    # Check for None parameters for lowpass, highpass, or band pass filter
-    
-    # l_freq
-    if config['param_filter_l_freq'] == "":
-        config['param_filter_l_freq'] = None  # when App is run on Bl, no value for this parameter corresponds to ''
-    
-    # h_freq
-    if config['param_filter_h_freq'] == "":
-        config['param_filter_h_freq'] = None  # when App is run on Bl, no value for this parameter corresponds to ''
-
-    # picks
-    if config['param_filter_picks'] == "":
-        config['param_filter_picks'] = None  # when App is run on Bl, no value for this parameter corresponds to ''
-
-    # iir parameters
-    if config['param_filter_iir_params'] == "":
-        config['param_filter_iir_params'] = None  # when App is run on Bl, no value for this parameter corresponds to ''
-
-    # Info message about filtering
-
-    # Band pass filter
-    if config['param_filter_l_freq'] is not None and config['param_filter_h_freq'] is not None:
-        comments_about_filtering = f'Data was filtered between ' \
-                                   f'{config["param_filter_l_freq"]} ' \
-                                   f'and {config["param_filter_h_freq"]}Hz'
-        dict_json_product['brainlife'].append({'type': 'info', 'msg': comments_about_filtering})
-
-    # Lowpass filter
-    elif config['param_filter_l_freq'] is None and config['param_filter_h_freq'] is not None:
-        comments_about_filtering = f'Lowpass filter was applied at {config["param_filter_h_freq"]}Hz'
-        dict_json_product['brainlife'].append({'type': 'info', 'msg': comments_about_filtering})
-
-    # Highpass filter
-    elif config['param_filter_l_freq'] is not None and config['param_filter_h_freq'] is None:
-        comments_about_filtering = f'Highpass filter was applied at {config["param_filter_l_freq"]}Hz'
-        dict_json_product['brainlife'].append({'type': 'info', 'msg': comments_about_filtering})
-
-    # Raise an exception if both param_filter_l_freq and param_filter_h_freq are None
-    elif config['param_filter_l_freq'] is None and config["param_filter_h_freq"] is None:
-        value_error_message = f'You must specify a value for param_filter_l_freq or param_filter_h_freq, ' \
-                              f"they can't both be set to None"
-        # Raise exception
-        raise ValueError(value_error_message)
-
-    # Info message about notch filtering if applied
-    if config['param_apply_notch'] is True:
-        dict_json_product['brainlife'].append({'type': 'info', 'msg': 'Notch filter was applied.'})
-        comments_notch = f"{config['param_notch_freqs_start']}Hz and its harmonics"
-
-        # Check for None parameters 
-
-        # picks notch
-        if config['param_notch_picks'] == "":
-            config['param_notch_picks'] = None  # when App is run on Bl, no value for this parameter corresponds to ''
-
-        # notch widths
-        if config['param_notch_widths'] == "":
-            config['param_notch_widths'] = None  # when App is run on Bl, no value for this parameter corresponds to ''  
-
-        # iir parameters
-        if config['param_notch_iir_parameters'] == "":
-            config['param_notch_iir_parameters'] = None  # when App is run on Bl, no value for this parameter corresponds to ''  
-
-        # mt bandwidth
-        if config['param_notch_mt_bandwidth'] == "":
-            config['param_notch_mt_bandwidth'] = None  # when App is run on Bl, no value for this parameter corresponds to ''         
-
+    if config['param_epoched_data'] is False:
+        data = mne.io.read_raw_fif(data_file, allow_maxshield=True)
     else:
-        comments_notch = 'No Notch filter was applied'
+        data = mne.read_epochs(data_file)
+
+    # Read events file 
+    events_file = config.pop('events')
+    if os.path.exists(events_file) is True:
+        shutil.copy2(events_file, 'out_dir_resampling/events.tsv')  # required to run a pipeline on BL
 
     # Info message about resampling if applied
-    if config['param_apply_resample'] is True:
-        if config['param_resample_sfreq'] is not None:
-            dict_json_product['brainlife'].append({'type': 'info', 'msg': f'Data was resampled at '
-                                                                          f'{config["param_resample_sfreq"]}. '
-                                                                          f'Please bear in mind that it is generally '
-                                                                          f'recommended not to epoch '
-                                                                          f'downsampled data, but instead epoch '
-                                                                          f'and then downsample.'})
-            comments_resample_freq = f'{config["param_resample_sfreq"]}Hz'
+    if config['param_epoched_data'] is False:
+        dict_json_product['brainlife'].append({'type': 'info', 'msg': f'Data was resampled at '
+                                                                      f'{config["param_resample_sfreq"]}. '
+                                                                      f'Please bear in mind that it is generally '
+                                                                      f'recommended not to epoch '
+                                                                      f'downsampled data, but instead epoch '
+                                                                      f'and then downsample.'})
+    
+    # Comment about resampling
+    comments_resample_freq = f'{config["param_resample_sfreq"]}Hz'
 
-            # Check for None parameters
+    # Check for None parameters
 
-            # stim picks
-            if config['param_resample_stim_picks'] == "":
-              config['param_resample_stim_picks'] = None  # when App is run on Bl, no value for this parameter corresponds to ''  
+    # stim picks
+    if config['param_resample_stim_picks'] == "":
+        config['param_resample_stim_picks'] = None  # when App is run on Bl, no value for this parameter corresponds to ''  
 
-            if config['param_resample_events'] == "":
-              config['param_resample_events'] = None  # when App is run on Bl, no value for this parameter corresponds to '' 
+    if config['param_resample_events'] == "":
+        config['param_resample_events'] = None  # when App is run on Bl, no value for this parameter corresponds to '' 
             
-        else:
-            value_error_message = f"You must specify a value for param_resample_sfreq. If you don't want to resample " \
-                                  f'your data, please set param_apply_resample to False.'
-            # Raise exception
-            raise ValueError(value_error_message)
-    else:
-        comments_resample_freq = 'Data was not resampled'
-
     # Keep bad channels in memory
     bad_channels = raw.info['bads']
 
@@ -439,20 +283,20 @@ def main():
     kwargs = config  
 
     # Apply temporal filtering
-    raw_copy = raw.copy()
-    raw_filtered = temporal_filtering(raw_copy, **kwargs)
-    del raw_copy
+    data_copy = data.copy()
+    data_filtered = resampling(data_copy, **kwargs)
+    del data_copy
 
     # Success message in product.json    
-    dict_json_product['brainlife'].append({'type': 'success', 'msg': 'Filtering was applied successfully.'})
+    dict_json_product['brainlife'].append({'type': 'success', 'msg': 'Data was successfully resampled.'})
 
     # Compute SNR
-    snr_before = _compute_snr(raw)
-    snr_after = _compute_snr(raw_filtered)
+    # snr_before = _compute_snr(raw)
+    # snr_after = _compute_snr(raw_filtered)
 
     # Generate a report
-    _generate_report(data_file, raw, raw_filtered, bad_channels, comments_about_filtering,
-                     comments_notch, comments_resample_freq, snr_before, snr_after)
+    # _generate_report(data_file, raw, raw_filtered, bad_channels, comments_about_filtering,
+    #                  comments_notch, comments_resample_freq, snr_before, snr_after)
 
     # Save the dict_json_product in a json file
     with open('product.json', 'w') as outfile:
